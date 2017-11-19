14a5459f730663507558fe29eb92e75e1e0b1447
abbydarda <darda.abby@gmail.com>
2017-11-20 01:23:44 +0700

    selesai

--- primality.php
+++ primality.php
@@ -1,24 +1,25 @@
 <?php
-$n = 5;
-$count=0;
-
-if($n==2){
-    $count=2;
-}elseif($n%2==0 && $n%3==0){
-        $count!=2;
-    }else{
-        for($i = 1; $i <= $n; $i++){
-            if($count>2){
-                break;
-            }else{
-                if($n%$i==0){
-                $count++;
-            }
-            }
-        }
+$a = [34,35,36,37];
+for ($i=0; $i < count($a); $i++) {
+  if ($a[$i]<=1) {
+    $bil = 0;
+  }elseif($a[$i]<=3) {
+    $bil = 1;
+  }elseif ($a[$i]%2==0) {
+    $bil =0;
+  }else {
+    for($j=2; $j<=sqrt($a[$i]);$j++){
+      if($a[$i]%$j==0){
+        $bil = 0;
+        break;
+      }else {
+        $bil = 1;
+      }
     }
-if($count==2){
-    echo "Prime\n";
-}else{
+  }
+  if ($bil==0) {
     echo "Not prime\n";
+  }else {
+    echo "Prime\n";
+  }
 }
