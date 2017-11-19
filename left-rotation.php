5837ec15575fd649ba411fc3ebba122d12a598ed
abbydarda <darda.abby@gmail.com>
2017-11-19 20:53:56 +0700

    selesai

--- left-rotation.php
+++ left-rotation.php
@@ -1,4 +1,14 @@
 <?php
-$array = array(1, 2, 3, 4, 5);
-print_r($array);
+$a = array(1, 2, 3, 4, 5);
+$n = count($a);
+$k = 4;
+
+$dump=[];
+for($i=0;$i < $n; $i++){
+    $new = ($i + $k)%$n;
+    $dump[$i] = $a[$new];
+}
+for($i=0;$i<$n;$i++){
+echo $dump[$i]." ";
+}
  ?>
