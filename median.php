04a3b8c44bc4b324b5a406546ede1f6bb1b7cad5
abbydarda <darda.abby@gmail.com>
2017-12-15 13:42:15 +0700

    median

--- median.php
+++ median.php
@@ -1,13 +1,20 @@
 <?php
-$a = [4,12,5];
-if(count($a)%2==0){
-  $median1 = (count($a)/2)-1;
-  $median2 = (count($a)/2);
-  $m = ($a[$median1]+$a[$median2])/2;
-  echo $m;
-}else{
-  $median = (count($a)/2)+0.5;
-  $m = $a[$median-1];
-  echo $m;
+$arr = [12,4,5];
+$c=0;
+$hasil=NULL;
+for ($i=0; $i <= count($arr); $i++) {
+  for ($j=0; $j < $i ; $j++) {
+    if ($c%2!=0) {
+      $median = (($c+1)/2)-1;
+      $hasil =  $arr[$median];
+    }else {
+      $d = $c/2;
+      $e = ($c/2)-1;
+      $median = ($arr[$d]+$arr[$e])/2;
+      $hasil =  $median;
+    }
+  }
+  echo $hasil;
+  echo "\n";
+  $c++;
 }
- ?>
